205b82172e78a176a0d9e1f3c77737c5eaf2bbc7
wutao <wutao@qq.com>
2022-11-05 13:17:32 +0800

    fix(全局): 提交代码 2022/11/05 13:17:32

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue';
+import 'virtual:windi-base.css';
+import 'virtual:windi-components.css';
+import 'virtual:windi-utilities.css';
 import './style.css';
 import App from './App.vue';
 
